9ff3ca85b2df751abe596659af02c9670a35c87e
Madhunath <146817676+Madhunath@users.noreply.github.com>
2024-03-06 14:44:11 +0530

    creating infrastructure using terraform

---
+++ main.tf
@@ -0,0 +1,134 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+# Create a new VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "my_vpc"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# Create a subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "my_route_table"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Create a security group
+resource "aws_security_group" "my_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8070
+    to_port     = 8070
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ICMP (ping)
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_security_group"
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "my_instance" {
+  count  =  2
+  ami                    = "ami-0440d3b780d96b29d" # Example AMI ID, replace with a valid one
+  instance_type          = "t2.micro"
+  key_name               = "awskey" # Replace with your key pair name
+  subnet_id              = aws_subnet.my_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  tags = {
+    Name = count.index == 0 ? "CI/CD-server" : "production-server"
+  }
+}
